ba4afe1fd2e064e1c2f5ec33216c8fdd6429d455
ericallenhess <72704859+ericallenhess@users.noreply.github.com>
2026-03-18 18:10:07 -0400

    Update UNOZoneMotionDriver.groovy

--- drivers/UNOZoneMotionDriver.groovy
+++ drivers/UNOZoneMotionDriver.groovy
@@ -1,2 +1,13 @@
-metadata { definition(name: "UNO Zone Motion", namespace: "uno", author: "Eric Hess") { capability "Sensor"; capability "MotionSensor"; attribute "zoneStateDigit", "string" } }
-def installed() { sendEvent(name: "motion", value: "inactive"); sendEvent(name: "zoneStateDigit", value: "0") }
+metadata {
+    definition(name: "UNO Zone Motion", namespace: "uno", author: "Eric Hess") {
+        capability "Sensor"
+        capability "MotionSensor"
+
+        attribute "zoneStateDigit", "string"
+    }
+}
+
+def installed() {
+    sendEvent(name: "motion", value: "inactive")
+    sendEvent(name: "zoneStateDigit", value: "0")
+}
