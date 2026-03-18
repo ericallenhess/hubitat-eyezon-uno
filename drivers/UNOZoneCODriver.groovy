52665ba70594473213d87194d6d3e9f57aa49407
ericallenhess <72704859+ericallenhess@users.noreply.github.com>
2026-03-18 18:07:22 -0400

    Update UNOZoneCODriver.groovy

--- drivers/UNOZoneCODriver.groovy
+++ drivers/UNOZoneCODriver.groovy
@@ -1,2 +1,13 @@
-metadata { definition(name: "UNO Zone CO", namespace: "uno", author: "Eric Hess") { capability "Sensor"; capability "CarbonMonoxideDetector"; attribute "zoneStateDigit", "string" } }
-def installed() { sendEvent(name: "carbonMonoxide", value: "clear"); sendEvent(name: "zoneStateDigit", value: "0") }
+metadata {
+    definition(name: "UNO Zone CO", namespace: "uno", author: "Eric Hess") {
+        capability "Sensor"
+        capability "CarbonMonoxideDetector"
+
+        attribute "zoneStateDigit", "string"
+    }
+}
+
+def installed() {
+    sendEvent(name: "carbonMonoxide", value: "clear")
+    sendEvent(name: "zoneStateDigit", value: "0")
+}
