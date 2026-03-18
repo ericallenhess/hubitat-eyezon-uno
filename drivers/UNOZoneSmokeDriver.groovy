f7c1f4a4e3635ccef9cc110b395803299186bbf4
ericallenhess <72704859+ericallenhess@users.noreply.github.com>
2026-03-18 18:11:06 -0400

    Update UNOZoneSmokeDriver.groovy

--- drivers/UNOZoneSmokeDriver.groovy
+++ drivers/UNOZoneSmokeDriver.groovy
@@ -1,2 +1,13 @@
-metadata { definition(name: "UNO Zone Smoke", namespace: "uno", author: "Eric Hess") { capability "Sensor"; capability "SmokeDetector"; attribute "zoneStateDigit", "string" } }
-def installed() { sendEvent(name: "smoke", value: "clear"); sendEvent(name: "zoneStateDigit", value: "0") }
+metadata {
+    definition(name: "UNO Zone Smoke", namespace: "uno", author: "Eric Hess") {
+        capability "Sensor"
+        capability "SmokeDetector"
+
+        attribute "zoneStateDigit", "string"
+    }
+}
+
+def installed() {
+    sendEvent(name: "smoke", value: "clear")
+    sendEvent(name: "zoneStateDigit", value: "0")
+}
