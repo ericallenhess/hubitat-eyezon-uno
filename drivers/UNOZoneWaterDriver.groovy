59a6b685567f58747b627869a2e4be4795160c58
ericallenhess <72704859+ericallenhess@users.noreply.github.com>
2026-03-18 18:12:05 -0400

    Update UNOZoneWaterDriver.groovy

--- drivers/UNOZoneWaterDriver.groovy
+++ drivers/UNOZoneWaterDriver.groovy
@@ -1,2 +1,13 @@
-metadata { definition(name: "UNO Zone Water", namespace: "uno", author: "Eric Hess") { capability "Sensor"; capability "WaterSensor"; attribute "zoneStateDigit", "string" } }
-def installed() { sendEvent(name: "water", value: "dry"); sendEvent(name: "zoneStateDigit", value: "0") }
+metadata {
+    definition(name: "UNO Zone Water", namespace: "uno", author: "Eric Hess") {
+        capability "Sensor"
+        capability "WaterSensor"
+
+        attribute "zoneStateDigit", "string"
+    }
+}
+
+def installed() {
+    sendEvent(name: "water", value: "dry")
+    sendEvent(name: "zoneStateDigit", value: "0")
+}
