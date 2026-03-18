e33fb7e7ae6a959a27946cf8429093fddb38052b
ericallenhess <72704859+ericallenhess@users.noreply.github.com>
2026-03-18 18:08:45 -0400

    Update UNOZoneContactDriver.groovy

--- drivers/UNOZoneContactDriver.groovy
+++ drivers/UNOZoneContactDriver.groovy
@@ -1,2 +1,13 @@
-metadata { definition(name: "UNO Zone Contact", namespace: "uno", author: "Eric Hess") { capability "Sensor"; capability "ContactSensor"; attribute "zoneStateDigit", "string" } }
-def installed() { sendEvent(name: "contact", value: "closed"); sendEvent(name: "zoneStateDigit", value: "0") }
+metadata {
+    definition(name: "UNO Zone Contact", namespace: "uno", author: "Eric Hess") {
+        capability "Sensor"
+        capability "ContactSensor"
+
+        attribute "zoneStateDigit", "string"
+    }
+}
+
+def installed() {
+    sendEvent(name: "contact", value: "closed")
+    sendEvent(name: "zoneStateDigit", value: "0")
+}
